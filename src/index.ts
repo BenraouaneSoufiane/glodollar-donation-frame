6c288411ee0aebd88f1bbd02ddcc1665d6f56e45
Benraouane Soufiane <110255764+BenraouaneSoufiane@users.noreply.github.com>
2024-07-06 07:04:06 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -34,85 +34,85 @@ app = Express();
 app.use(Express.urlencoded({ extended: true }));
 app.use(Express.static('public'));
 app.use(Express.json());
-app.get('/',async(req,res)=>{ 
+app.get('/glodollar',async(req,res)=>{ 
     res.send(`<!DOCTYPE html><html><head>
       <title>Glo Dollar Donation Frame</title>
-      <meta property="og:image" content="https://glodollar.cryptocheckout.co/intro.png" />
+      <meta property="og:image" content="https://frames.cryptocheckout.co/glodollar-intro.png" />
       <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="https://glodollar.cryptocheckout.co/intro.png" />
+      <meta property="fc:frame:image" content="https://frames.cryptocheckout.co/glodollar-intro.png" />
       <meta property="fc:frame:button:1" content="Yes" />
       <meta property="fc:frame:button:1:action" content="post" />
-      <meta property="fc:frame:button:1:post_url" content="https://glodollar.cryptocheckout.co/poll" />
-      <meta property="fc:frame:button:1:target" content="https://glodollar.cryptocheckout.co/poll" />
+      <meta property="fc:frame:button:1:post_url" content="https://frames.cryptocheckout.co/glodollar/poll" />
+      <meta property="fc:frame:button:1:target" content="https://frames.cryptocheckout.co/glodollar/poll" />
       <meta property="fc:frame:button:2" content="No" />
       <meta property="fc:frame:button:2:action" content="post" />
-      <meta property="fc:frame:button:2:post_url" content="https://glodollar.cryptocheckout.co/outro" />
-      <meta property="fc:frame:button:2:target" content="https://glodollar.cryptocheckout.co/outro" />
+      <meta property="fc:frame:button:2:post_url" content="https://frames.cryptocheckout.co/glodollar/outro" />
+      <meta property="fc:frame:button:2:target" content="https://frames.cryptocheckout.co/glodollar/outro" />
       </head></html>`);
   });
-  app.post('/',async(req,res)=>{ 
+  app.post('/glodollar',async(req,res)=>{ 
   
     res.send(`<!DOCTYPE html><html><head>
       <title>Glo Dollar Donation Frame</title>
-      <meta property="og:image" content="https://glodollar.cryptocheckout.co/intro.png" />
+      <meta property="og:image" content="https://frames.cryptocheckout.co/glodollar-intro.png" />
       <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="https://glodollar.cryptocheckout.co/intro.png" />
+      <meta property="fc:frame:image" content="https://frames.cryptocheckout.co/glodollar-intro.png" />
       <meta property="fc:frame:button:1" content="Yes" />
       <meta property="fc:frame:button:1:action" content="post" />
-      <meta property="fc:frame:button:1:post_url" content="https://glodollar.cryptocheckout.co/poll" />
+      <meta property="fc:frame:button:1:post_url" content="https://frames.cryptocheckout.co/glodollar/poll" />
       <meta property="fc:frame:button:2" content="No" />
       <meta property="fc:frame:button:2:action" content="post" />
-      <meta property="fc:frame:button:2:post_url" content="https://glodollar.cryptocheckout.co/outro" />
+      <meta property="fc:frame:button:2:post_url" content="https://frames.cryptocheckout.co/glodollar/outro" />
       </head></html>`);
   });
-  app.post('/outro',async(req,res)=>{
+  app.post('/glodollar/outro',async(req,res)=>{
   
     res.send(`<!DOCTYPE html><html><head>
       <title>Glo Dollar Donation Frame</title>
-      <meta property="og:image" content="https://glodollar.cryptocheckout.co/outro.png" />
+      <meta property="og:image" content="https://frames.cryptocheckout.co/glodollar-outro.png" />
       <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="https://glodollar.cryptocheckout.co/outro.png" />
-      <meta property="fc:frame:post_url" content="https://glodollar.cryptocheckout.co" />
+      <meta property="fc:frame:image" content="https://frames.cryptocheckout.co/glodollar-outro.png" />
+      <meta property="fc:frame:post_url" content="https://frames.cryptocheckout.co" />
       <meta property="fc:frame:button:1" content="Restart" />
       <meta property="fc:frame:button:1:action" content="post" />
       </head></html>`);
   });
-  app.post('/poll',async(req,res)=>{ 
+  app.post('/glodollar/poll',async(req,res)=>{ 
   
     res.send(`<!DOCTYPE html><html><head>
       <title>Glo Dollar Donation Frame</title>
-      <meta property="og:image" content="https://glodollar.cryptocheckout.co/select.png" />
+      <meta property="og:image" content="https://frames.cryptocheckout.co/glodollar-select.png" />
       <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="https://glodollar.cryptocheckout.co/select.png" />
+      <meta property="fc:frame:image" content="https://frames.cryptocheckout.co/glodollar-select.png" />
       <meta property="fc:frame:button:1" content="💸 Give Directly" />
       <meta property="fc:frame:button:1:action" content="tx" />
-      <meta property="fc:frame:button:1:target" content="https://glodollar.cryptocheckout.co/get_tx_data" />
-      <meta property="fc:frame:button:1:post_url" content="https://glodollar.cryptocheckout.co/tx_callback" />
+      <meta property="fc:frame:button:1:target" content="https://frames.cryptocheckout.co/get_tx_data" />
+      <meta property="fc:frame:button:1:post_url" content="https://frames.cryptocheckout.co/tx_callback" />
       <meta property="fc:frame:button:2" content="🌳 Giving Green" />
       <meta property="fc:frame:button:2:action" content="tx" />
-      <meta property="fc:frame:button:2:target" content="https://glodollar.cryptocheckout.co/get_tx_data" />
-      <meta property="fc:frame:button:2:post_url" content="https://glodollar.cryptocheckout.co/tx_callback" />
+      <meta property="fc:frame:button:2:target" content="https://frames.cryptocheckout.co/get_tx_data" />
+      <meta property="fc:frame:button:2:post_url" content="https://frames.cryptocheckout.co/tx_callback" />
       <meta property="fc:frame:button:3" content="🐹 Animal Charity Evaluators" />
       <meta property="fc:frame:button:3:action" content="tx" />
-      <meta property="fc:frame:button:3:target" content="https://glodollar.cryptocheckout.co/get_tx_data" />
-      <meta property="fc:frame:button:3:post_url" content="https://glodollar.cryptocheckout.co/tx_callback" />
+      <meta property="fc:frame:button:3:target" content="https://frames.cryptocheckout.co/get_tx_data" />
+      <meta property="fc:frame:button:3:post_url" content="https://frames.cryptocheckout.co/tx_callback" />
       <meta property="fc:frame:button:4" content="✌️ UNHCR" />
       <meta property="fc:frame:button:4:action" content="tx" />
-      <meta property="fc:frame:button:4:target" content="https://glodollar.cryptocheckout.co/get_tx_data" />
-      <meta property="fc:frame:button:4:post_url" content="https://glodollar.cryptocheckout.co/tx_callback" />
+      <meta property="fc:frame:button:4:target" content="https://frames.cryptocheckout.co/get_tx_data" />
+      <meta property="fc:frame:button:4:post_url" content="https://frames.cryptocheckout.co/tx_callback" />
       <meta property="fc:frame:input:text" content="Please enter amount in USDGLO" />
       
       </head></html>`);
   });
   
  
-  app.post('/tx_callback',async(req,res)=>{ 
+  app.post('/glodollar/tx_callback',async(req,res)=>{ 
     console.log(req.body);
     res.send(`<!DOCTYPE html><html><head>
       <title>Glo Dollar Donation Frame</title>
-      <meta property="og:image" content="https://glodollar.cryptocheckout.co/share.png" />
+      <meta property="og:image" content="https://frames.cryptocheckout.co/glodollar-share.png" />
       <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="https://glodollar.cryptocheckout.co/share.png" />
+      <meta property="fc:frame:image" content="https://frames.cryptocheckout.co/glodollar-share.png" />
       <meta property="fc:frame:button:1" content="Share" />
       <meta property="fc:frame:button:1:action" content="link" />
       <meta property="fc:frame:button:1:post_url" content="https://warpcast.com/~/compose?text=Hello%20world!" />
@@ -130,7 +130,7 @@ app.get('/',async(req,res)=>{
     gasPrice?: Number;
     maxPriorityFeePerGas?: Number
   }  
-app.post('/get_tx_data',async function (_req: Express.Request, res: TransactionTargetResponse <{chainId: string;
+app.post('/glodollar/get_tx_data',async function (_req: Express.Request, res: TransactionTargetResponse <{chainId: string;
     method: "eth_sendTransaction";
     attribution?: boolean;
     params: EthSendTransactionParams 
